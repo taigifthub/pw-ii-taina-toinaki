650a24b599c7cc9149185296ea25bc4de9fbb16f
taigifthub <tainamarcondes07@gmail.com>
2024-03-13 10:29:27 -0300

    Aula sobre variáveis e estrutura de decisão

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pw-ii-taina-toinaki
+Repositório para as aulas de Web com os professores Palhares e João na 
+Etec Camargo Aranha
---
+++ aula01/variaveis.php
@@ -0,0 +1,10 @@
+<?php 
+  $texto = "isso é um texto";
+  $n1 = 2;
+  $n2 = 3;
+
+  echo $texto;
+  echo "<br>";
+  echo gettype($texto), "<br>";
+  echo gettype($n1);
+?>
